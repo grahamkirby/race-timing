a76fbf07c3014619b41f1a868b068dc6fe371617
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2023-11-07 21:52:09 +0000

    Progress.

--- src/main/java/lap_race/LegResult.java
+++ src/main/java/lap_race/LegResult.java
@@ -3,30 +3,31 @@ package lap_race;
 import java.time.Duration;
 import java.util.Map;
 
-public class LegResult {
+public class LegResult implements Comparable<LegResult> {
 
     int leg_number;
-    int bib_number;
-    String team_name;
-    Category team_category;
+    Team team;
     boolean DNF;
 
     Duration recorded_split_time;
     Duration adjusted_split_time;
     Duration adjustment_for_finishing_after_next_leg_mass_start; // Zero if it isn't after mass start.
-    Duration leg_time;
+    Duration leg_duration;
+    Duration finish_time; // Relative to start of leg 1.
 
-    public LegResult(int leg_number, int bib_number, Duration recorded_split_time, Duration adjusted_split_time, Duration adjustment_for_finishing_after_next_leg_mass_start, Duration leg_time, Map<Integer, Team> entries) {
+    public LegResult() {
+
+    }
+
+    public LegResult(int leg_number, Team team, Duration recorded_split_time, Duration adjusted_split_time, Duration adjustment_for_finishing_after_next_leg_mass_start, Duration leg_time, Map<Integer, Team> entries) {
 
         this.leg_number = leg_number;
-        this.bib_number = bib_number;
+        this.team = team;
         this.recorded_split_time = recorded_split_time;
         this.adjusted_split_time = adjusted_split_time;
         this.adjustment_for_finishing_after_next_leg_mass_start = adjustment_for_finishing_after_next_leg_mass_start;
-        this.leg_time = leg_time;
+        this.leg_duration = leg_time;
 
-        team_name = getTeamName(bib_number, entries);
-        team_category = getTeamCategory(bib_number, entries);
         DNF = false;
     }
 
@@ -45,6 +46,11 @@ public class LegResult {
     }
 
     public String toString() {
-        return bib_number + "," + team_name + "," + team_category + "," + OverallResult.format(recorded_split_time) + "," + OverallResult.format(adjustment_for_finishing_after_next_leg_mass_start) + "," + OverallResult.format(leg_time);
+        return team.runners[leg_number - 1] + "," + OverallResult.format(leg_duration);
+    }
+
+    @Override
+    public int compareTo(LegResult o) {
+        return leg_duration.compareTo(o.leg_duration);
     }
 }
