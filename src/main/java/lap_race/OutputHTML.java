b42c9644157b397daa43f12fc54ebbc313c27a3d
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2024-02-01 19:40:38 +0000

    Added combined HTML output.

--- src/main/java/lap_race/OutputHTML.java
+++ src/main/java/lap_race/OutputHTML.java
@@ -1,6 +1,7 @@
 package lap_race;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,39 +21,68 @@ public class OutputHTML extends Output {
     @Override
     public void printOverallResults() throws IOException {
 
-        final Path overall_results_html_path = output_directory_path.resolve(overall_results_filename + ".html");
+        final OutputStream stream = Files.newOutputStream(output_directory_path.resolve(overall_results_filename + ".html"));
 
-        try (final OutputStreamWriter html_writer = new OutputStreamWriter(Files.newOutputStream(overall_results_html_path))) {
-
-            printOverallResultsHeader(html_writer);
+        try (final OutputStreamWriter html_writer = new OutputStreamWriter(stream)) {
             printOverallResults(html_writer);
-            printOverallResultsFooter(html_writer);
         }
     }
 
     @Override
     public void printDetailedResults() throws IOException {
 
-        final Path detailed_results_html_path = output_directory_path.resolve(detailed_results_filename + ".html");
+        final OutputStream stream = Files.newOutputStream(output_directory_path.resolve(detailed_results_filename + ".html"));
 
-        try (final OutputStreamWriter html_writer = new OutputStreamWriter(Files.newOutputStream(detailed_results_html_path))) {
-
-            printDetailedResultsHeader(html_writer);
+        try (final OutputStreamWriter html_writer = new OutputStreamWriter(stream)) {
             printDetailedResults(html_writer);
-            printDetailedResultsFooter(html_writer);
         }
     }
 
     public void printLegResults(final int leg) throws IOException {
 
-        final Path leg_results_html_path = output_directory_path.resolve(race_name_for_filenames + "_leg_" + leg + "_" + year + ".html");
+        final OutputStream stream = Files.newOutputStream(output_directory_path.resolve(race_name_for_filenames + "_leg_" + leg + "_" + year + ".html"));
 
-        try (final OutputStreamWriter html_writer = new OutputStreamWriter(Files.newOutputStream(leg_results_html_path))) {
-
-            printLegResultsHeader(html_writer, leg);
-            printLegResults(html_writer, getLegResults(leg));
-            printLegResultsFooter(html_writer);
+        try (final OutputStreamWriter html_writer = new OutputStreamWriter(stream)) {
+            printLegResults(html_writer, leg);
         }
+    }
+
+    public void printCombined() throws IOException {
+
+        final OutputStream stream = Files.newOutputStream(output_directory_path.resolve("combined.html"));
+
+        try (final OutputStreamWriter html_writer = new OutputStreamWriter(stream)) {
+
+            html_writer.append("""
+                    <h3><strong>Results</strong></h3>
+                    <h4>Overall</h4>
+                    """);
+
+            printOverallResults(html_writer);
+
+            html_writer.append("""
+                    <h4>Full Results</h4>
+                    """);
+
+            printDetailedResults(html_writer);
+
+            html_writer.append("""
+                    <p>M3: mass start leg 3<br />M4: mass start leg 4</p>
+                    """);
+
+            for (int leg_number = 1; leg_number <= results.number_of_legs; leg_number++) {
+
+                html_writer.append("<p></p>\n<h4>Leg ").append(String.valueOf(leg_number)).append(" Results</h4>\n");
+                printLegResults(html_writer, leg_number);
+            }
+        }
+    }
+
+    private void printOverallResults(OutputStreamWriter html_writer) throws IOException {
+
+        printOverallResultsHeader(html_writer);
+        printOverallResultsBody(html_writer);
+        printOverallResultsFooter(html_writer);
     }
 
     private void printOverallResultsHeader(final OutputStreamWriter writer) throws IOException {
@@ -72,7 +102,7 @@ public class OutputHTML extends Output {
             """);
     }
 
-    private void printOverallResults(final OutputStreamWriter writer) throws IOException {
+    private void printOverallResultsBody(final OutputStreamWriter writer) throws IOException {
 
         int position = 1;
 
@@ -112,6 +142,13 @@ public class OutputHTML extends Output {
             """);
     }
 
+    private void printDetailedResults(OutputStreamWriter html_writer) throws IOException {
+
+        printDetailedResultsHeader(html_writer);
+        printDetailedResultsBody(html_writer);
+        printDetailedResultsFooter(html_writer);
+    }
+
     private void printDetailedResultsHeader(final OutputStreamWriter writer) throws IOException {
 
         writer.append("""
@@ -126,7 +163,10 @@ public class OutputHTML extends Output {
 
         for (int leg_number = 1; leg_number <= results.number_of_legs; leg_number++) {
 
-            writer.append("<th>Runners ").append(String.valueOf(leg_number)).append("</th>");
+            writer.append("<th>Runner");
+            if (results.paired_legs[leg_number-1]) writer.append("s");
+            writer.append(" ").append(String.valueOf(leg_number)).append("</th>");
+
             writer.append("<th>Leg ").append(String.valueOf(leg_number)).append("</th>");
 
             if (leg_number < results.number_of_legs)
@@ -142,7 +182,7 @@ public class OutputHTML extends Output {
             """);
     }
 
-    private void printDetailedResults(final OutputStreamWriter writer) throws IOException {
+    private void printDetailedResultsBody(final OutputStreamWriter writer) throws IOException {
 
         for (int result_index = 0; result_index < results.overall_results.length; result_index++)
             printDetailedResult(writer, result_index);
@@ -177,6 +217,21 @@ public class OutputHTML extends Output {
                 </tr>""");
     }
 
+   private void printDetailedResultsFooter(final OutputStreamWriter writer) throws IOException {
+
+        writer.append("""
+                </tbody>
+            </table>
+            """);
+    }
+
+    private void printLegResults(final OutputStreamWriter html_writer, int leg) throws IOException {
+
+        printLegResultsHeader(html_writer, leg);
+        printLegResultsBody(html_writer, getLegResults(leg));
+        printLegResultsFooter(html_writer);
+    }
+
     private void printLegDetails(OutputStreamWriter writer, OverallResult result, Team team) throws IOException {
 
         boolean any_previous_leg_dnf = false;
@@ -207,14 +262,6 @@ public class OutputHTML extends Output {
         }
     }
 
-    private void printDetailedResultsFooter(final OutputStreamWriter writer) throws IOException {
-
-        writer.append("""
-                </tbody>
-            </table>
-            """);
-    }
-
     private void printLegResultsHeader(final OutputStreamWriter writer, final int leg) throws IOException {
 
         writer.append("""
@@ -235,7 +282,7 @@ public class OutputHTML extends Output {
             """);
     }
 
-    private void printLegResults(final OutputStreamWriter writer, final LegResult[] leg_results) throws IOException {
+    private void printLegResultsBody(final OutputStreamWriter writer, final LegResult[] leg_results) throws IOException {
 
         for (final LegResult leg_result : leg_results) {
 
