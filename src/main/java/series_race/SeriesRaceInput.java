64237b14fa38d6f32172316b20dc9fedeac6cd27
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2024-06-02 22:13:16 +0100

    Refactoring.

--- src/main/java/series_race/SeriesRaceInput.java
+++ src/main/java/series_race/SeriesRaceInput.java
@@ -6,54 +6,17 @@ import fife_ac_races.Midweek;
 import individual_race.IndividualRace;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class SeriesRaceInput extends RaceInput {
 
-    public SeriesRaceInput(final Midweek race) {
+    public SeriesRaceInput(final Race race) {
 
         super(race);
     }
 
-//    @Override
-//    public void readProperties() {
-//
-//        race_config_paths = readRaceConfigPaths();
-//    }
-//
-//    private Path[] readRaceConfigPaths() {
-//
-//        final String[] race_strings = race.getProperties().getProperty("RACES").split(":", -1);
-//
-//        final Path[] race_paths = new Path[race_strings.length];
-//
-//        for (int i = 0; i < race_paths.length; i++)
-//            race_paths[i] = Paths.get(race_strings[i]);
-//
-//        return race_paths;
-//    }
+    @Override
+    protected void configureIndividualRace(final IndividualRace individual_race, final int race_number) throws IOException {
 
-    public IndividualRace[] loadSeriesRaces() throws IOException {
-
-        final IndividualRace[] races = new IndividualRace[race_config_paths.length];
-
-        for (int i = 0; i < races.length; i++) {
-
-            final Path relative_path = race_config_paths[i];
-
-            if (!relative_path.toString().isEmpty()) {
-
-                final Path individual_race_path = race.getWorkingDirectoryPath().resolve(relative_path);
-
-                IndividualRace individual_race = new IndividualRace(individual_race_path);
-                individual_race.configure();
-                individual_race.processResults(false);
-
-                races[i] = individual_race;
-            }
-        }
-
-        return races;
+        individual_race.configure();
     }
 }
