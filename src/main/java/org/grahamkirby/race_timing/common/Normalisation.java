a3599b9eb3642fcfb9d3097a362d7e385da6d63a
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2024-10-10 09:37:00 +0100

    Testing key changes.

--- src/main/java/org/grahamkirby/race_timing/common/Normalisation.java
+++ src/main/java/org/grahamkirby/race_timing/common/Normalisation.java
@@ -39,7 +39,7 @@ public class Normalisation {
         String[] words = input.split(" ");
 
         for (String word : words) {
-            if (word.length() > 0) {
+            if (!word.isEmpty()) {
                 titleCase.append(Character.toUpperCase(word.charAt(0)))
                         .append(word.substring(1).toLowerCase())
                         .append(" ");
