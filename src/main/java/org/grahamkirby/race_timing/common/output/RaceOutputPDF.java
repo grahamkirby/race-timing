5311ae6e2d83fed2c51ff4fdbe68f40ec70c562b
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2025-06-19 09:59:01 +0100

    Fixed licence header check on build.

--- src/main/java/org/grahamkirby/race_timing/common/output/RaceOutputPDF.java
+++ src/main/java/org/grahamkirby/race_timing/common/output/RaceOutputPDF.java
@@ -1,20 +1,22 @@
 /*
- * Copyright 2025 Graham Kirby:
- * <https://github.com/grahamkirby/race-timing>
+ * race-timing - <https://github.com/grahamkirby/race-timing>
+ * Copyright © 2025 Graham Kirby (graham.kirby@st-andrews.ac.uk)
  *
- * This file is part of the module race-timing.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * race-timing is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
- * version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * race-timing is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with race-timing. If not, see
- * <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.grahamkirby.race_timing.common.output;
+
 
 import com.itextpdf.io.font.constants.StandardFonts;
 import com.itextpdf.kernel.font.PdfFont;
