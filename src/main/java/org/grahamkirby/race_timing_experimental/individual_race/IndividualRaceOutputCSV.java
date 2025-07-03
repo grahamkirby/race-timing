3f16176d42a5acd218bc72fce7f3d7ffebaf97b9
Graham Kirby <graham.kirby@st-andrews.ac.uk>
2025-07-03 13:13:58 +0100

    Investigating Windows build problem.

--- src/main/java/org/grahamkirby/race_timing_experimental/individual_race/IndividualRaceOutputCSV.java
+++ src/main/java/org/grahamkirby/race_timing_experimental/individual_race/IndividualRaceOutputCSV.java
@@ -108,6 +108,7 @@ class IndividualRaceOutputCSV {
      */
     Path getOutputFilePath(final String race_name, final String output_type, final String year) {
         Path resolve = race.getFullPath("../output").resolve(STR."\{race_name}_\{output_type}_\{year}.csv");
+       IndividualRaceOutputText. debug_info = resolve.toString();
         return resolve;
     }
 
